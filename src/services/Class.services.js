e6ad4f3b5113e5efd90e7e80e71f4e9a74b38932
Carlos Rodriguez <carlos97sdg@gmail.com>
2023-11-28 12:37:40 +0100

    class services

---
+++ src/services/Class.services.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+
+class ClassServices {
+    constructor() {
+        this.api = axios.create({
+            baseURL: `${import.meta.env.VITE_API_URL}/class`
+        })
+
+        this.api.interceptors.request.use((config) => {
+
+            const storedToken = localStorage.getItem("authToken");
+
+            if (storedToken) {
+                config.headers = { Authorization: `Bearer ${storedToken}` }
+            }
+
+            return config
+        })
+
+    }
+
+    create(classData) {
+        return this.api.post("/create", classData)
+    }
+    getClasses() {
+        return this.api.get("/all")
+    }
+    getOneClass(class_id) {
+        return this.api.get(`/${class_id}`)
+    }
+}
+
+const classService = new ClassServices()
+
+export default classService

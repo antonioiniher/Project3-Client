9c2f1d15d07829b3d959a32a10a2e5a00b07fe3b
Pablo Portillo <paabloportillo@gmail.com>
2023-11-29 12:56:48 +0100

    Cliente Pablo

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,19 +1,35 @@
 
 import { Form, Row } from "react-bootstrap"
+import { useState } from "react";
 
 
-const SearchBar = () => {
 
 
-    return (
-        <Form>
-            <Row>
-                <Form.Group className="mb-3 searchBar" controlId="searcher" style={{ marginLeft: "50px", padding: "30px" }}>
-                    <Form.Control type="text" placeholder="Busca una clase" />
-                </Form.Group>
-            </Row>
-        </Form >
-    )
+const SearchBar = ({ setResults }) => {
+
+  const [query, setQuery] = useState('');
+
+  const items = ['JavaScript', 'Ruby', 'C++', 'Java', 'Python']
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+    getFilteredItems(e.target.value)
+  };
+
+  const getFilteredItems = (value) => {
+    if (!value) setResults([])
+    else setResults(items.filter(e => e.toLowerCase().startsWith(value.toLowerCase())))
+  }
+
+  return (
+    <Form>
+      <Row>
+        <Form.Group className="mb-3 searchBar" controlId="searcher" style={{ marginLeft: "50px", padding: "30px" }}>
+          <Form.Control type="text" placeholder="Busca una clase" value={query} onChange={handleSearch} />
+        </Form.Group>
+      </Row>
+    </Form >
+  )
 
 }
 

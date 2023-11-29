a50d2b13359938338a7f234cab19362b3541767b
Carlos Rodriguez <carlos97sdg@gmail.com>
2023-11-29 23:45:11 +0100

    homepage and howwork

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,29 +1,33 @@
 import './Footer.css'
 import { Link } from 'react-router-dom'
 import { Row, Col, Container } from 'react-bootstrap'
+
+import iconInbox from "../../assets/icon-inbox.png"
 const Footer = () => {
 
     return (
         <footer>
             <Container>
-                <div className="border-top pb-3 mb-3">
+                <div className="border-top pb-3 mb-4 pt-2">
                     <Row>
-                        <Col md={{ offset: 2 }}>
+                        <Col md={{ offset: 1 }}>
+                            <span className="text-center linksFooter" >© 2023 Proyecto 3</span>
+                        </Col>
+                        <Col md={{ offset: 1 }}>
                             <span><Link to={'/'} className="linksFooter">Home </Link></span>
                         </Col>
-                        <Col md={{ offset: 2 }}>
+                        <Col md={{ offset: 1 }}>
                             <span><Link to={'/nosotros'} className="linksFooter">Nosotros </Link></span>
                         </Col>
-                        <Col md={{ offset: 2 }}>
+                        <Col md={{ offset: 1 }}>
                             <span><Link to={'/como-funciona'} className="linksFooter">Como funciona </Link></span>
                         </Col>
-                        <Col md={{ offset: 2 }}>
+                        <Col md={{ offset: 1 }}>
                             <span><Link to={'/contacto'} className="linksFooter">Contacto </Link></span>
                         </Col>
                     </Row>
 
                 </div>
-                <p className="text-center linksFooter">© 2023 Proyecto 3</p>
             </Container>
         </footer>
     )

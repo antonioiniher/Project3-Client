3f82a6810bec1d112c31bdd58e42403c3a5a4e40
Antonio Iñiguez <antonioiniher@icloud.com>
2023-11-29 23:46:01 +0100

    maquetado

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,11 +1,7 @@
 import "./ContactPage.css"
-import { Row, Col, Container } from "react-bootstrap"
+import { Row, Col, Container, Form } from "react-bootstrap"
 import { Link } from "react-router-dom"
-import iconFacebook from "../../assets/iconFacebook.png"
-import iconLinkedin from "../../assets/iconLinkedin.png"
-import iconTwitter from "../../assets/iconTwitter.png"
-import iconInstagram from "../../assets/iconInstagram.png"
-
+import icon from '../../assets/icon-send.svg'
 
 const ContactPage = () => {
 
@@ -13,34 +9,42 @@ const ContactPage = () => {
     return (
         <div>
             <Container className="contactContainer">
-                <Row>
-                    <Col md={{ span: 6 }}>
-                        <h1 className="titleContactPage">Contáctanos</h1>
-                        <p className="subtitleContactPage"> y conecta con expertos en programación</p>
-                    </Col>
-                </Row>
-                <Row className="iconsSocials">
-                    <Col>
-                        <Link to={"https://www.facebook.com/?locale=es_ES"}>
-                            <img src={iconFacebook} alt="iconFacebook" className="imgFacebook" />
-                        </Link>
-                    </Col>
-                    <Col>
-                        <Link to={"https://www.linkedin.com/"}>
-                            <img src={iconLinkedin} alt="iconLinkedin" style={{ height: "50px" }} />
-                        </Link>
-                    </Col>
-                    <Col>
-                        <Link to={"https://twitter.com/?lang=es"}>
-                            <img src={iconTwitter} alt="iconTwitter" style={{ height: "50px" }} />
-                        </Link>
-                    </Col>
-                    <Col>
-                        <Link to={"https://www.instagram.com/"}>
-                            <img src={iconInstagram} alt="iconInstagram" style={{ height: "50px" }} />
-                        </Link>
-                    </Col>
-                </Row>
+                <div>
+
+                </div>
+                <div>
+                    <h1 className="titleContact">Contáctanos</h1>
+                </div>
+                <div>
+                    <h2 className="sendMessage">Mándanos un mensaje</h2>
+                </div>
+                <div className="contactForm">
+                    <Row className="mt-5">
+                        <Col md={{ span: 6 }}>
+                            <p>Tu nombre *</p>
+                            <Form.Group className="">
+                                <Form.Control className="contactControl" type="text" name="name" placeholder="Escribe tu nombre" />
+                            </Form.Group>
+                        </Col>
+                        <Col md={{ span: 6 }}>
+                            <p>Tu email *</p>
+                            <Form.Group className="">
+                                <Form.Control className="contactControl" type="text" name="email" placeholder="Escribe tu email" />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row className="mt-5">
+                        <Col>
+                            <p>Tu mensaje *</p>
+                            <Form.Group className="">
+                                <Form.Control className="contactControl" type="text" name="message" placeholder="Escribe tus necesidades" />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                </div>
+                <div>
+                    <Link className="formButton btn">Enviar mensaje <img src={icon} alt="" className="buttonImg" /></Link>
+                </div>
             </Container>
 
         </div>

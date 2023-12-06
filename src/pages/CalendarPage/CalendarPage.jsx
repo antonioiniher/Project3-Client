ec1fd708a6ac04a960b2b972178902950e460f98
Pablo Portillo <paabloportillo@gmail.com>
2023-12-06 17:28:57 +0100

    pablo cliente

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -1,12 +1,13 @@
 import Calendar from './../../components/Calendar/Calendar'
 import { Container } from "react-bootstrap"
+import './CalendarPage.css'
 
 
 const CalendarPage = () => {
     return (
         <div className="CalendarPage">
             <Container>
-                <h1>Calendario</h1>
+                <h1 className='classTitle'>Calendario</h1>
                 <hr />
                 <Calendar />
             </Container>
